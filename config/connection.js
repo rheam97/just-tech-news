af3d0d7adf8925b71a35549b9d596dbd1541f1cc
Rhea <rheam97@gmail.com>
2022-02-14 15:05:17 -0500

    installed sequalize, made skeleton for files, created CRUD routes, user class, excluded passwords

---
+++ config/connection.js
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize')
+
+require('dotenv').config()
+const sequelize = new Sequelize('just_tech_news_db', 'root', '', {
+    host: 'localhost',  
+    dialect: 'mysql',
+    port: 3306
+})
+
+module.exports = sequelize
