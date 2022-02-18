f5ebd2113f157356b594680aaf59e6e45f618468
Rhea <rheam97@gmail.com>
2022-02-18 14:36:09 -0500

    issues with express static

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
+const path = require('path')
 const express = require('express')
-
 const routes = require('./routes')
 
 const sequelize = require('./config/connection')
@@ -7,8 +7,10 @@ const sequelize = require('./config/connection')
 const app = express()
 const PORT = process.env.PORT || 3001
 
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(routes)
 
