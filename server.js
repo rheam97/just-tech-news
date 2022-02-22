f641f1c1efc7a7025fd229dbe6b3684da4fe6e6b
Rhea <rheam97@gmail.com>
2022-02-22 04:58:14 -0500

    trying to figure out stylesheet issue

--- server.js
+++ server.js
@@ -23,13 +23,13 @@ const { response } = require('express')
 const app = express()
 const PORT = process.env.PORT || 3001
 
-
-app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 app.use(express.static(path.join(__dirname, '/public')))
 app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
 app.use(session(sess))
+
 
 app.use(routes)
 
